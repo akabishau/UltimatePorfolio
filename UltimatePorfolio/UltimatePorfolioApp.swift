3ad828afa7a7a16b3de287eb23d9ade27ff9fb65
akabishau <aleksey.kabishau@icloud.com>
2023-02-18 12:46:03 -0500

    Initial Commit

---
+++ UltimatePorfolio/UltimatePorfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  UltimatePorfolioApp.swift
+//  UltimatePorfolio
+//
+//  Created by Aleksey on 2/18/23.
+//
+
+import SwiftUI
+
+@main
+struct UltimatePorfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
